bd38ed3546d119570e605a49ae09fd114f0c5f7e
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2022-09-26 13:36:44 -0300

    codigo semelhante na HiJobBase

--- EsqueletoBatch/HiBatch/HiJobBase.cs
+++ EsqueletoBatch/HiBatch/HiJobBase.cs
@@ -1,9 +1,32 @@
+using Microsoft.Extensions.Configuration;
+
 namespace EsqueletoBatch.HiBatch;
 public class HiJobBase
 {
     protected readonly Guid _guid;
-    public HiJobBase()
+    protected readonly IHiLogger _hiLogger;
+    protected readonly IConfigurationRoot _config;
+    public HiJobBase(IHiLogger hiLogger, IConfigurationRoot config)
     {
         _guid = Guid.NewGuid();
+        _hiLogger = hiLogger;
+        _config = config;
+    }
+    protected void ImprimirInicioExec(string tituloExec)
+    {
+        _hiLogger.ImprimirLinha("<> [ EXECUÇÃO: " + tituloExec + " ] </>");
+        _hiLogger.ImprimirLinha("    [ Guid ] " + _guid);
+        _hiLogger.ImprimirLinha("    [ Hora do Início ] " + DateTime.Now);
+        _hiLogger.ImprimirLinha("    [ Início da Execução... ]");
+    }
+    protected void ImprimirFimExec()
+    {
+        _hiLogger.ImprimirLinha("    [ Fim da Execução ]");
+        _hiLogger.ImprimirLinha("    [ Hora do Fim ] " + DateTime.Now);
+    }
+    protected void ImprimirErro(Exception ex)
+    {
+        _hiLogger.ImprimirLinha("    [ Ocorreu um erro ]");
+        _hiLogger.ImprimirLinha("        " + ex.Message);
     }
 }
--- EsqueletoBatch/Job/SalvarSomaJob.cs
+++ EsqueletoBatch/Job/SalvarSomaJob.cs
@@ -7,35 +7,24 @@ namespace EsqueletoBatch.Job;
 public class SalvarSomaJob : HiJobBase, IJob
 {
     private readonly ISomarServico _somarServico;
-    private readonly IHiLogger _hiLogger;
-    private readonly IConfigurationRoot _config;
-    public SalvarSomaJob(ISomarServico somarServico, IHiLogger hiLogger, IConfigurationRoot config) : base()
+    public SalvarSomaJob(ISomarServico somarServico, IHiLogger hiLogger, IConfigurationRoot config) : base(hiLogger, config)
     {
         _somarServico = somarServico;
-        _hiLogger = hiLogger;
-        _config = config;
     }
     public async Task Execute(IJobExecutionContext context)
     {
         try
         {
-            _hiLogger.ImprimirLinha("<> [ EXECUÇÃO: Exportação da Soma ] </>");
-            _hiLogger.ImprimirLinha("    [ Guid ] " + _guid);
-            _hiLogger.ImprimirLinha("    [ Hora do Início ] " + DateTime.Now);
-            _hiLogger.ImprimirLinha("    [ Início da Execução... ]");
-
+            ImprimirInicioExec("Exportação da Soma");
             var n1 = int.Parse(_config.GetSection("Jobs:SalvarSomaJob:Params:N1").Value);
             var n2 = int.Parse(_config.GetSection("Jobs:SalvarSomaJob:Params:N2").Value);
             var caminhoCompletoArqSalvar = _config.GetSection("Jobs:SalvarSomaJob:Params:CaminhoCompletoArqSalvar").Value;
             _somarServico.SalvarSoma(n1, n2, caminhoCompletoArqSalvar);
-
-            _hiLogger.ImprimirLinha("    [ Fim da Execução ]");
-            _hiLogger.ImprimirLinha("    [ Hora do Fim ] " + DateTime.Now);
+            ImprimirFimExec();
         }
         catch (Exception ex)
         {
-            _hiLogger.ImprimirLinha("    [ Ocorreu um erro ]");
-            _hiLogger.ImprimirLinha("        " + ex.Message);
+            ImprimirErro(ex);
             throw;
         }
     }
